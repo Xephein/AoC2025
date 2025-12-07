35777cc959a81d129c707e6c5e07c774eb3c99fd
Xephein <friesz.marton@gmail.com>
2025-12-07 12:31:56 +0100

    D3 P2

--- day3.py
+++ day3.py
@@ -23,6 +23,32 @@ def part_1(bank):
             largest = battery
     return int(tens+str(largest))
 
+def part_2(bank):
+    joltage = ""
+    length = len(joltage)
+    joltage += find_largest(bank, length, joltage)
+    return int(joltage)
+
+def find_largest(bank_section, length, joltage):
+    if len(joltage) == 12:
+        return ""
+    if len(bank_section) == 12 - len(joltage):
+        return bank_section
+    length = -(11 - len(joltage)) if len(joltage) < 11 else None
+    largest = float("-inf")
+    position = 0
+    for i in range(len(bank_section[:length])):
+        battery = int(bank_section[i])
+        if battery > largest:
+            largest = battery
+            position = i
+    largest = str(largest)
+    joltage += largest
+    length = len(joltage)
+    largest += find_largest(bank_section[position + 1:], length, joltage)
+    return largest
+
+
 t0 = time()
 
 content = ut.parse_input("day3.txt")
@@ -34,3 +60,15 @@ for line in content:
 t1 = time()
 
 ut.log_result("part 1", answer, t1-t0)
+
+t0 = time()
+
+content = ut.parse_input("day3.txt")
+answer = 0
+
+for line in content:
+    answer += int(part_2(line))
+
+t1 = time()
+
+ut.log_result("part 2", answer, t1-t0)
